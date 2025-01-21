c2fd210df1e341574714c73036858244f4259795
Braslyvm <braslymorales@gmail.com>
2025-01-20 22:08:15 -0600

    login

---
+++ TallerMecanicoWeb/src/DB/Autentificacion.jsx
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+
+export const app = firebase.initializeApp({
+  projectId: "fb-authentication-549f5",
+  appId: "1:51010532888:web:71b6d9df0cf632bb58f56d",
+  storageBucket: "fb-authentication-549f5.firebasestorage.app",
+  apiKey: "AIzaSyCzSBR6H4upvesPbsKhNQ90nUig9D0-Yws",
+  authDomain: "fb-authentication-549f5.firebaseapp.com",
+  messagingSenderId: "51010532888",
+});
+
---
+++ TallerMecanicoWeb/src/pages/Login.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import 'firebase/compat/auth';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
+import { app } from '../DB/Autentificacion'; 
+import firebase from 'firebase/compat/app'
+
+
+
+export default function Logeo() {
+  const [email, setEmailLocal] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          navigate('/');
+        })
+        .catch((error) => {
+          setError(error);
+        });
+    } else {
+      setError(error);
+    }
+  };
+
+  return (
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      backgroundColor: '#ffffff', 
+      padding: '0 20px',
+      overflow: 'hidden',
+      marginTop: '-100px',
+      marginLeft: '-250px',
+    }}>
+      <CssBaseline />
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5"   sx={{ marginRight: '5%' }}>
+        {"Iniciar Sesión"}
+        </Typography>
+      {error && <Typography color="error" variant="body2" sx={{ mt: 2 }}>{error}</Typography>} 
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, width: '100%', maxWidth: '400px' }}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label={"Correo Electrónico"}
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={email}
+          onChange={(e) => setEmailLocal(e.target.value)}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label={"Contraseña"}
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          {"Iniciar Sesión"}
+        </Button>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <Link href="/Registro" variant="body2">
+              {"¿No tienes una cuenta? Regístrate"}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+}
