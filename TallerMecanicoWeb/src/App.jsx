7ae1374db5c99eb36c38d25e52457a07a9895fcb
Braslyvm <braslymorales@gmail.com>
2025-01-26 18:31:04 -0600

    actualización del siderban

--- TallerMecanicoWeb/src/App.jsx
+++ TallerMecanicoWeb/src/App.jsx
@@ -3,19 +3,15 @@ import './App.css';
 import { MyRoutes } from './routers/routes.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import styled from 'styled-components';
-import Sidebar from './components/Sidebar';
+import MySidebar from './components/Sidebar'; // Asegúrate de que este sea el correcto
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'rsuite/dist/rsuite.min.css';
 
 function App() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <BrowserRouter>
-      <AppContainer className={sidebarOpen ? 'active' : ''}>
-        {/* Sidebar */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-        {/* Rutas de la aplicación */}
+      <AppContainer>
+        <MySidebar />
         <Content>
           <MyRoutes />
         </Content>
@@ -28,27 +24,15 @@ export default App;
 
 // Contenedor principal de la aplicación
 const AppContainer = styled.div`
-  display: grid;
-  grid-template-columns: ${({ className }) =>
-    className === 'active' ? '300px auto' : '60px auto'};
-  transition: grid-template-columns 0.3s ease-in-out;
-  height: 100%;
-  overflow-y: auto; /* Permite el desplazamiento vertical si el contenido es mayor que el contenedor */
-  width: 100%;
-  grid-template-rows: auto 1fr; /* Agrega esta línea para que el contenido ocupe más espacio */
+  display: flex; // Cambia a flex para que el sidebar y el contenido se alineen horizontalmente
+  height: 100vh; // Asegúrate de que el contenedor principal ocupe toda la altura de la ventana
+  overflow: hidden; // Evita el scroll en el contenedor principal
 `;
-
-
 
 // Contenedor para el contenido principal (rutas)
 const Content = styled.div`
-  background-color:rgb(253, 251, 251);
+  background-color: rgb(253, 251, 251);
   padding: 20px;
   overflow-y: auto;
-  height: 100%
-  width: 100%
-  display: flex; /* Añadí un display flex para mejorar la organización */
-  justify-content: flex-start; /* Cambié de center a flex-start para alinear el contenido a la izquierda */
-  align-items: flex-start; /* Asegura que el contenido no esté centrado verticalmente */
+  flex: 1; // Permite que el contenido ocupe el espacio restante
 `;
-
