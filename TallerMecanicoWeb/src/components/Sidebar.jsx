539f18c22045167f8f3011c5fdc7560c3ae210fd
Braslyvm <braslymorales@gmail.com>
2025-01-27 21:38:02 -0600

    actualización del sidebar

--- TallerMecanicoWeb/src/components/Sidebar.jsx
+++ TallerMecanicoWeb/src/components/Sidebar.jsx
@@ -1,20 +1,17 @@
-
 // Sidebar.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Sidenav, Nav, Toggle } from 'rsuite';
-import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
-import GroupIcon from '@rsuite/icons/legacy/Group';
-import MagicIcon from '@rsuite/icons/legacy/Magic';
-import HomeIcon from '@rsuite/icons/legacy/Home';
-import ToolsIcon from '@rsuite/icons/Tools';
-import RateIcon from '@rsuite/icons/Rate';
-import CarIcon from "@rsuite/icons/legacy/Car";
-import BarChartHorizontalIcon from '@rsuite/icons/BarChartHorizontal';
-import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
+import { Sidenav, Nav } from 'rsuite';
 import { FaBars } from 'react-icons/fa'; 
 import { useNavigate } from 'react-router-dom';
+import HomeIcon from '@rsuite/icons/legacy/Home';
+import GroupIcon from '@rsuite/icons/legacy/Group';
+import ToolsIcon from '@rsuite/icons/Tools';
+import BarChartHorizontalIcon from '@rsuite/icons/BarChartHorizontal';
+import RateIcon from '@rsuite/icons/Rate';
+import CarIcon from "@rsuite/icons/legacy/Car";
+import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
 import CreditCardPlusIcon from '@rsuite/icons/CreditCardPlus';
+import './sidebar.css'; // Importa el archivo CSS
 
 const MySidebar = () => {
   const [expanded, setExpanded] = React.useState(true);
@@ -30,7 +27,6 @@ const MySidebar = () => {
     setActiveKey(eventKey);
   };
 
-  // Manejador de la apertura y cierre de menús
   const handleMenuToggle = (eventKey) => {
     if (!expanded) {
       setExpanded(true); 
@@ -38,12 +34,13 @@ const MySidebar = () => {
 
     setOpenedMenu(openedMenu === eventKey ? null : eventKey); 
   };
+
   return (
     <div
       style={{
         width: expanded ? 240 : 55, 
         height: '100vh',
-        backgroundColor: '#ffffff', 
+        backgroundColor: '#27374D', // Cambiado a #27374D
         transition: 'width 0.3s ease',
         overflowY: 'auto', 
         overflowX: 'hidden', 
@@ -53,63 +50,61 @@ const MySidebar = () => {
         onClick={() => setExpanded(!expanded)}
         style={{ 
           fontSize: '24px', 
-          color: '#000', 
+          color: '#ffffff', // Cambiado a blanco
           cursor: 'pointer', 
           margin: '10px',
-          backgroundColor: expanded ? '#ffffff' : '#f0f0f0', 
+          backgroundColor: '#27374D', // Cambiado a #27374D
           borderRadius: '5px', 
           padding: '5px' 
         }}
       />
-      <hr />
+      <hr style={{ borderColor: '#ffffff' }} /> {/* Cambiado a blanco */}
       <Sidenav
         expanded={expanded}
-        defaultOpenKeys={['3', '4']}
+        defaultOpenKeys={['3']}
         style={{
           height: 'calc(100vh - px)', 
           overflowY: 'auto',
           overflowX: 'hidden', 
-          backgroundColor: expanded ? '#ffffff' : '#ffffff'
-          
+          backgroundColor: '#27374D' // Cambiado a #27374D
         }}
       >
-        <Sidenav.Body style={{ backgroundColor: '#ffffff' }}> 
-          <Nav activeKey={activeKey} onSelect={handleNavSelect} >
-            <Nav.Item style={{ backgroundColor: '#ffffff' }} eventKey="1" icon={<HomeIcon /> } onClick={() => {navigate("/");}}>
+        <Sidenav.Body style={{ backgroundColor: '#27374D' }}> {/* Cambiado a #27374D */}
+          <Nav activeKey={activeKey} onSelect={handleNavSelect}>
+            <Nav.Item eventKey="1" icon={<HomeIcon style={{ color: '#ffffff' }} />} onClick={() => {navigate("/");}}>
               Inicio
             </Nav.Item>
-            <Nav.Item style={{ backgroundColor: '#ffffff' }}eventKey="2" icon={<GroupIcon />} onClick={() => {navigate("/Mecanicos");}}>
+            <Nav.Item eventKey="2" icon={<GroupIcon style={{ color: '#ffffff' }} />} onClick={() => {navigate("/Mecanicos");}}>
               Planilla
             </Nav.Item>
             <Nav.Menu
               placement="rightStart"
               eventKey="3"
               title="solicitudes"
-              icon={<ToolsIcon />}
+              icon={<ToolsIcon style={{ color: '#ffffff' }} />}
               onToggle={() => handleMenuToggle('3')} 
               open={openedMenu === '3'} 
-              style={{ backgroundColor: '#ffffff' }} 
             >
-              <Nav.Item  style={{ backgroundColor: '#ffffff' }} eventKey="3-1"  onClick={() => {navigate("/Reparaciones");}}>Reparaciones</Nav.Item>
-              <Nav.Item style={{ backgroundColor: '#ffffff' }}  eventKey="3-2">Devices</Nav.Item>
-              <Nav.Item style={{ backgroundColor: '#ffffff' }}  eventKey="3-3">Loyalty</Nav.Item>
-              <Nav.Item style={{ backgroundColor: '#ffffff' }}  eventKey="3-4">Visit Depth</Nav.Item>
+              <Nav.Item eventKey="3-1" onClick={() => {navigate("/Reparaciones");}}>Reparaciones</Nav.Item>
+              <Nav.Item eventKey="3-2">Devices</Nav.Item>
+              <Nav.Item eventKey="3-3">Loyalty</Nav.Item>
+              <Nav.Item eventKey="3-4">Visit Depth</Nav.Item>
             </Nav.Menu>
-            <Nav.Item style={{ backgroundColor: '#ffffff' }} eventKey="4" icon={<BarChartHorizontalIcon />} onClick={() => {navigate("/HReparaciones");}}>
+            <Nav.Item eventKey="4" icon={<BarChartHorizontalIcon style={{ color: '#ffffff' }} />} onClick={() => {navigate("/HReparaciones");}}>
               Historial de Reparaciones
             </Nav.Item>
-            <Nav.Item style={{ backgroundColor: '#ffffff' }} eventKey="5" icon={<RateIcon />} onClick={() => {navigate("/Registro_repuesto");}}>
+            <Nav.Item eventKey="5" icon={<RateIcon style={{ color: '#ffffff' }} />} onClick={() => {navigate("/Registro_repuesto");}}>
               Repuesto
             </Nav.Item>
-            <Nav.Item style={{ backgroundColor: '#ffffff' }}eventKey="6" icon={<CarIcon />} onClick={() => {navigate("/Diagnostico");}}>
+            <Nav.Item eventKey="6" icon={<CarIcon style={{ color: '#ffffff' }} />} onClick={() => {navigate("/Diagnostico");}}>
               Diagnósticos
             </Nav.Item>
-            <Nav.Item style={{ backgroundColor: '#ffffff' }}eventKey="7" icon={<GearCircleIcon />} onClick={() => {navigate("/Mecanicos");}}>
+            <Nav.Item eventKey="7" icon={<GearCircleIcon style={{ color: '#ffffff' }} />} onClick={() => {navigate("/Mecanicos");}}>
               Ajustes
             </Nav.Item>
-            <Nav.Item style={{ backgroundColor: '#ffffff' }}eventKey="8" icon={<CreditCardPlusIcon />} onClick={() => {navigate("/Facturas");}}>
+            <Nav.Item eventKey="8" icon={<CreditCardPlusIcon style={{ color: '#ffffff' }} />} onClick={() => {navigate("/Facturas");}}>
               Facturar
-            </Nav.Item>
+            </Nav.Item>
           </Nav>
         </Sidenav.Body>
       </Sidenav>
