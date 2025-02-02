59f58e5954be64335231ad103fa09b7ed5ae7beb
Braslyvm <braslymorales@gmail.com>
2025-02-02 16:48:25 -0600

    traduccion al sidebar

--- TallerMecanicoWeb/src/components/Sidebar.jsx
+++ TallerMecanicoWeb/src/components/Sidebar.jsx
@@ -14,12 +14,14 @@ import CarIcon from "@rsuite/icons/legacy/Car";
 import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
 import CreditCardPlusIcon from '@rsuite/icons/CreditCardPlus';
 import './sidebar.css'; // Importa el archivo CSS
+import { useGlobalContext } from './GlobalContext';
 
 const MySidebar = () => {
   const [expanded, setExpanded] = React.useState(true);
   const navigate = useNavigate(); 
   const [activeKey, setActiveKey] = React.useState('1');
   const [openedMenu, setOpenedMenu] = React.useState(null); 
+  const { translate } = useGlobalContext();
 
   const handleNavSelect = (eventKey) => {
     if (!expanded && (eventKey === '3' || eventKey === '4')) {
@@ -38,10 +40,22 @@ const MySidebar = () => {
   };
 
   const handleLogout = () => {
-    // Aquí puedes implementar la lógica de cierre de sesión
-    // Por ejemplo, eliminar el token de autenticación y redirigir al usuario
-    // localStorage.removeItem('token'); // Si usas localStorage
-    navigate('/'); // Redirigir a la página de inicio de sesión
+    navigate('/'); 
+  };
+
+  const translatedContent = {
+    Inicio: translate ? 'Home' : 'Inicio',
+    Planilla: translate ? 'Sheet' : 'Planilla',
+    Diagnósticos: translate ? 'Diagnostics' : 'Diagnósticos',
+    Solicitudes: translate ? 'Requests' : 'Solicitudes',
+    Todaslasreparaciones: translate ? 'All Repairs' : 'Todas las reparaciones',
+    Reparacionesencurso: translate ? 'Repairs in progress' : 'Reparaciones en curso',
+    Historialdereparaciones: translate ? 'Repair History' : 'Historial de reparaciones',
+    Cerrarsesión: translate ? 'Logout' : 'Cerrar sesión',
+    Repuesto: translate ? 'Replacement' : 'Repuesto',
+    Facturar: translate ? 'Invoice' : 'Facturar',
+    Ajustes: translate ? 'Settings' : 'Ajustes',
+    reparaciones: translate ? 'Repairs' : 'Reparaciones',
   };
 
   return (
@@ -81,35 +95,35 @@ const MySidebar = () => {
         <Sidenav.Body style={{ backgroundColor: '#27374D' }}> {/* Cambiado a #27374D */}
           <Nav activeKey={activeKey} onSelect={handleNavSelect}>
             <Nav.Item eventKey="1" icon={<HomeIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/Home");}}>
-              Inicio
+              {translatedContent.Inicio}
             </Nav.Item>
             <Nav.Item eventKey="2" icon={<GroupIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/Mecanicos");}}>
-              Planilla
+              {translatedContent.Planilla}
             </Nav.Item>
             <Nav.Item eventKey="3" icon={<CarIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/Diagnostico");}}>
-              Diagnósticos
+              {translatedContent.Diagnósticos}
             </Nav.Item>
             <Nav.Menu
               placement="rightStart"
               eventKey="4"
-              title="Reparaciones"
+              title={translatedContent.reparaciones}
               icon={<ToolsIcon style={{ color: '#ffffff', fontSize: '20px' }} />}
               onToggle={() => handleMenuToggle('4')} 
               open={openedMenu === '4'} 
             >
-              <Nav.Item eventKey="4-1" icon={<CiSquareQuestion style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/Reparaciones");}}>  Solicitudes</Nav.Item>
-              <Nav.Item eventKey="4-2" icon={<BiSolidCarMechanic style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={()=>{navigate("/ReparacionesCompletas")}}>  Todas las reparaciones</Nav.Item>
-              <Nav.Item eventKey="4-3" icon={<GiMechanicGarage style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={()=>{navigate("/ReparacionesCurso")}}>  Reparaciones en curso</Nav.Item>
-              <Nav.Item eventKey="4-4" icon={<BarChartHorizontalIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/HReparaciones");}}>  Historial de reparaciones</Nav.Item>
+              <Nav.Item eventKey="4-1" icon={<CiSquareQuestion style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/Reparaciones");}}>  {translatedContent.Solicitudes}</Nav.Item>
+              <Nav.Item eventKey="4-2" icon={<BiSolidCarMechanic style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={()=>{navigate("/ReparacionesCompletas")}}>  {translatedContent.Todaslasreparaciones}</Nav.Item>
+              <Nav.Item eventKey="4-3" icon={<GiMechanicGarage style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={()=>{navigate("/ReparacionesCurso")}}>  {translatedContent.Reparacionesencurso}</Nav.Item>
+              <Nav.Item eventKey="4-4" icon={<BarChartHorizontalIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/HReparaciones");}}> {translatedContent.Historialdereparaciones}</Nav.Item>
             </Nav.Menu>
             <Nav.Item eventKey="5" icon={<RateIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/Registro_repuesto");}}>
-              Repuesto
+            {translatedContent.Repuesto}
             </Nav.Item>
             <Nav.Item eventKey="6" icon={<CreditCardPlusIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/Facturas");}}>
-              Facturar
+            {translatedContent.Facturar}
             </Nav.Item>
             <Nav.Item eventKey="7" icon={<GearCircleIcon style={{ color: '#ffffff', fontSize: '20px' }} />} onClick={() => {navigate("/settings");}}>
-              Ajustes
+            {translatedContent.Ajustes}
             </Nav.Item>
             <hr style={{ borderColor: '#ffffff', margin: '20px 0' }} /> {/* Línea separadora */}
             <Nav.Item 
@@ -117,7 +131,7 @@ const MySidebar = () => {
               icon={<CiLogout style={{ color: '#ffffff', fontSize: '20px' }}  />}  onClick={handleLogout}
               style={{ display: 'flex', justifyContent: 'flex-start' }} // Alineación a la izquierda
             >
-              <span style={{ marginLeft: '10px' }}>Cerrar sesión</span> {/* Espaciado entre el ícono y el texto */}
+              <span style={{ marginLeft: '10px' }}>{translatedContent.Cerrarsesión}</span> {/* Espaciado entre el ícono y el texto */}
             </Nav.Item>
           </Nav>
         </Sidenav.Body>
