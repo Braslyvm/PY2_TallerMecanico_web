94a7f1fe31612155c1c74fe75eeab4201761aeeb
Braslyvm <braslymorales@gmail.com>
2025-02-02 16:31:46 -0600

    actualizar ajustes

--- TallerMecanicoWeb/src/pages/Settings.jsx
+++ TallerMecanicoWeb/src/pages/Settings.jsx
@@ -1,4 +1,3 @@
-// SettingsPage.js
 import React from 'react';
 import { useGlobalContext } from '../components/GlobalContext'; // Asegúrate de que la ruta sea correcta
 import './Settings.css'; // Asegúrate de importar el archivo CSS
@@ -16,76 +15,78 @@ function Settings() {
 
   return (
     <div className={`settings-container ${dark ? 'dark' : 'light'}`}>
-      <h1>Ajustes</h1>
-      <div>
-        <label className="switch">
-          <input id="input" type="checkbox" checked={dark} onChange={toggleDarkMode} />
-          Modo oscuro
-          <div className="slider round">
-            <div className="sun-moon">
-              <svg id="moon-dot-1" className="moon-dot" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="moon-dot-2" className="moon-dot" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="moon-dot-3" className="moon-dot" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="light-ray-1" className="light-ray" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="light-ray-2" className="light-ray" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="light-ray-3" className="light-ray" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="cloud-1" className="cloud-dark" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="cloud-2" className="cloud-dark" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="cloud-3" className="cloud-dark" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="cloud-4" className="cloud-light" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="cloud-5" className="cloud-light" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
-              <svg id="cloud-6" className="cloud-light" viewBox="0 0 100 100">
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
+        <h1>Ajustes</h1>
+        <div className="settings-content">
+            <div className="setting-item">
+                <label className="switch">
+                    <input id="input" type="checkbox" checked={dark} onChange={toggleDarkMode} />
+                    <span className="slider round">
+                        <div className="sun-moon">
+                            <svg id="moon-dot-1" className="moon-dot" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="moon-dot-2" className="moon-dot" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="moon-dot-3" className="moon-dot" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="light-ray-1" className="light-ray" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="light-ray-2" className="light-ray" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="light-ray-3" className="light-ray" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-1" className="cloud-dark" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-2" className="cloud-dark" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-3" className="cloud-dark" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-4" className="cloud-light" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-5" className="cloud-light" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                            <svg id="cloud-6" className="cloud-light" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50"></circle>
+                            </svg>
+                        </div>
+                        <div className="stars">
+                            <svg id="star-1" className="star" viewBox="0 0 20 20">
+                                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
+                            </svg>
+                            <svg id="star-2" className="star" viewBox="0 0 20 20">
+                                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
+                            </svg>
+                            <svg id="star-3" className="star" viewBox="0 0 20 20">
+                                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
+                            </svg>
+                            <svg id="star-4" className="star" viewBox="0 0 20 20">
+                                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
+                            </svg>
+                        </div>
+                    </span>
+                </label>
+                <span>Modo oscuro</span>
             </div>
-            <div className="stars">
-              <svg id="star-1" className="star" viewBox="0 0 20 20">
-                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-              </svg>
-              <svg id="star-2" className="star" viewBox="0 0 20 20">
-                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-              </svg>
-              <svg id="star-3" className="star" viewBox="0 0 20 20">
-                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-              </svg>
-              <svg id="star-4" className="star" viewBox="0 0 20 20">
-                <path d="M 0 10 C 10 10,10 10 ,0 10 C 10 10 , 10 10 , 10 20 C 10 10 , 10 10 , 20 10 C 10 10 , 10 10 , 10 0 C 10 10,10 10 ,0 10 Z"></path>
-              </svg>
+            <div className="setting-item">
+                <label className="switch">
+                    <input type="checkbox" checked={translate} onChange={toggleTranslate} />
+                    <span className="slider"></span>
+                </label>
+                <span>Traducir a Inglés</span>
             </div>
-          </div>
-        </label>
-      </div>
-      <div>
-        <label className="switch">
-          <input type="checkbox" checked={translate} onChange={toggleTranslate} />
-          <span className="slider"></span>
-        </label>
-        Traducir a Inglés
-      </div>
+        </div>
     </div>
-  );
-};
+);
+}
 
 export default Settings;
